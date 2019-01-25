d40e4ea1c21bbefcb76524528312921a98a8068e
jorzen1984 <277602146@qq.com>
2019-01-25 08:53:40 +0800

    初始化

---
+++ Peibankongjian/Controllers/AdminBaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SkyDal;
+
+namespace EduGroup.Controllers
+{
+    public class AdminBaseController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            if (Session["uname"] == null)
+            {
+                //filterContext.HttpContext.Response.Redirect("/User/Login");
+                filterContext.Result = Redirect("/AdminAccount/Login");
+            }
+            ViewBag.Title = "陪伴空间";
+        }
+    }
+}
