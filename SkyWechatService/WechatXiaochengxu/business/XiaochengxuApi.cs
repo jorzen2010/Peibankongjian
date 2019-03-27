7168126d0eadc7378a74c1666ff09e14b0b297b3
jorzen1984 <277602146@qq.com>
2019-03-27 21:21:13 +0800

    OK

---
+++ SkyWechatService/WechatXiaochengxu/business/XiaochengxuApi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using SkyCommon;
+
+namespace WechatXiaochengxu
+{
+    public class XiaochengxuApi
+    {
+        public static string GetOpenidByWxlogin(string appid, string secret, string js_code, string grant_type)
+        {
+            string userAgent = System.Web.HttpContext.Current.Request.UserAgent;
+            string url = string.Format("https://api.weixin.qq.com/sns/jscode2session?appid={0}&secret={1}&js_code={2}&grant_type={3}", appid, secret, js_code, grant_type);
+
+            HttpWebResponse res = HttpWebResponseUtility.CreateGetHttpResponse(url, null, userAgent, null);
+            Stream stream = res.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string result = sr.ReadToEnd();
+            return result;
+
+        }
+    }
+}
